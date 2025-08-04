68a15b9c39b287c579e285e0548d8bf8eb67b151
arnbasten <arnba@Maison_Pro>
2025-08-04 17:34:47 +0200

    chore: mise en place du MVC+ config d'express et dotenv

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+
+const app = express();
+dotenv.config ();
+
+const PORT= process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`démarrage de app ocoffee sur https://localhost:${PORT}`);
+});
