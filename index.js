c0e28873512a77345a5be68dbec7edd5eb0e945a
arnbasten <arnba@Maison_Pro>
2025-08-06 12:16:04 +0200

    css & img rétabli

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ import routes from './routes.js';
 const app = express();
 dotenv.config ();
 
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.use(routes);
+app.use(express.static('public'));
 
 const PORT= process.env.PORT || 4000;
 app.listen(PORT, () => {
